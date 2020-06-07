39e38f770dbada8d408b9e72276284c13794cd11
thefourthben <chouweimin@berkeley.edu>
2020-06-07 17:17:55 +0800

    complete onnx and runtime test, inputing entire eff model

--- earlystop.py
+++ earlystop.py
@@ -3,7 +3,7 @@ import torch
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0):
+    def __init__(self, patience=5, verbose=False, delta=0):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -34,4 +34,3 @@ class EarlyStopping:
         else:
             self.best_score = score
             self.save_checkpoint(val_loss, model)
-            self.counter = 0
--- train.py
+++ train.py
@@ -8,74 +8,97 @@ from torchvision import transforms, models
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn.functional as F
-
-# Defining global variables
-bacc = 0
-n_epoch = 30
-loss = t.nn.CrossEntropyLoss()
+from models.effnet import EfficientNet as eff
+from earlystop import EarlyStopping as early 
+import torch.onnx
+import onnx
+import onnxruntime
+from PIL import Image
+import collections
 
 
 class training():
-    def __init__(self):
+    def __init__(self, model):
         self.lr = 0.01
-        self.effnet_type = "efficientnet-b0"
-        self.classes = ('plane', 'car', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck')
         self.trainset = None
         self.trainloader = None
         self.valset = None
         self.valloader = None
         self.testset = None
         self.testloader = None
+        self.whichmodel = model
+        self.bacc = 0
+        self.n_epoch = 3
+        self.loss = t.nn.CrossEntropyLoss()
+        self.factor = 0.5
+        self.classes = None
+        BlockArgs = collections.namedtuple('BlockArgs', [
+            'kernel_size', 'num_repeat', 'input_filters', 'output_filters',
+            'expand_ratio', 'id_skip', 'strides', 'se_ratio'
+        ])
+        self.block = argument_block = [ 
+            BlockArgs(kernel_size=3, num_repeat=1, input_filters=32, output_filters=16, expand_ratio=1, id_skip=True, strides=[1, 1], se_ratio=0.25),
+            BlockArgs(kernel_size=3, num_repeat=2, input_filters=16, output_filters=24, expand_ratio=6, id_skip=True, strides=[2, 2], se_ratio=0.25),
+            BlockArgs(kernel_size=5, num_repeat=2, input_filters=24, output_filters=40, expand_ratio=6, id_skip=True, strides=[2, 2], se_ratio=0.25),
+            BlockArgs(kernel_size=3, num_repeat=3, input_filters=40, output_filters=80, expand_ratio=6, id_skip=True, strides=[2, 2], se_ratio=0.25),
+            BlockArgs(kernel_size=5, num_repeat=3, input_filters=80, output_filters=112, expand_ratio=6, id_skip=True, strides=[1, 1], se_ratio=0.25),
+            BlockArgs(kernel_size=5, num_repeat=4, input_filters=112, output_filters=192, expand_ratio=6, id_skip=True, strides=[2, 2], se_ratio=0.25),
+            BlockArgs(kernel_size=3, num_repeat=1, input_filters=192, output_filters=320, expand_ratio=6, id_skip=True, strides=[1, 1], se_ratio=0.25)
+            ]
 
     def preproc(self):
         transform_train = transforms.Compose([
-            transforms.Resize((32,32)),
+            transforms.Resize((224)),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-        trainset = tv.CIFAR10(root='./data', train=True,
+        self.trainset = tv.CIFAR10(root='./data', train=True,
                             download=True, transform=transform_train)
-        length = int(len(trainset) * 0.9)
-        trainset, valset = t.utils.data.random_split(trainset, [length, len(trainset) - length])
-        trainloader = t.utils.data.DataLoader(trainset, batch_size=128,
+        length = int(len(self.trainset) * 0.9)
+        self.trainset, self.valset = t.utils.data.random_split(self.trainset, [length, len(self.trainset) - length])
+        self.trainloader = t.utils.data.DataLoader(self.trainset, batch_size=80,
                                             shuffle=True, num_workers=2)
-        valloader = t.utils.data.DataLoader(valset, batch_size= 32, shuffle=True, num_workers=1)
-        testset = tv.CIFAR10(root='./data', train=False,
+        self.valloader = t.utils.data.DataLoader(self.valset, batch_size= 32, shuffle=True, num_workers=2)
+        self.testset = tv.CIFAR10(root='./data', train=False,
                             download=True, transform=transform_train)
-        testloader = t.utils.data.DataLoader(testset, batch_size=128,
+        self.testloader = t.utils.data.DataLoader(self.testset, batch_size=80,
                                             shuffle=True, num_workers=2)
 
     def train(self):
+        self.preproc()
+        self.classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
         # Getting the Model
-        model = eff(effnet_type, len(classes))
+        model = self.whichmodel(blocks_args=self.block, global_params=self.classes)
         # Unfreezing the model parameters
         for param in model.parameters():
             param.requires_grad = True
 
         # Setting up the Loss function, optimizer, and channels
-        optimizer = t.optim.SGD(model.parameters(), lr=init_lr, momentum=0.9, weight_decay=0.005)
+        optimizer = t.optim.SGD(model.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.00001)
         # optimizer = t.optim.RMSprop(model.parameters(), lr = init_lr, momentum=0.9, weight_decay=1e-5)
-        # scheduler = t.optim.lr_scheduler.CosineAnnealingLR(optimizer, n_epoch)
+        # optimizer = t.optim.Adam(model.parameters(), lr = self.lr, weight_decay=0.000125)
+        # scheduler = t.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.n_epoch)
         # scheduler = t.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.97)
-        scheduler = t.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=3, verbose=True)
+        scheduler = t.optim.lr_scheduler.ReduceLROnPlateau(optimizer,mode = "max",factor=self.factor,
+         patience=5, verbose=True)
         # setting the metric and confusion matrix
         lm = meter.AverageValueMeter()
         model.cuda()
-        for num in range (30):
+        for num in range (self.n_epoch):
             model.train()
             lm.reset()
             total = 0
             correct = 0
             train_loss = 0 
-            for i, (data, label) in enumerate(trainloader):
-                input = Variable(data).cuda()
+            for i, (data, label) in enumerate(self.trainloader):
+                inputs = Variable(data).cuda()
                 target = Variable(label).cuda()
                 # zero the parameter gradients
                 optimizer.zero_grad()
                 # forward + backward + optimize
-                outputs = model(input)
-                l = loss(outputs, target)
+                outputs = model(inputs)
+                l = self.loss(outputs, target)
                 train_loss = l
                 l.backward()
                 optimizer.step()
@@ -83,7 +106,7 @@ class training():
                 _, predicted = outputs.max(1)
                 total += target.size(0)
                 correct += predicted.eq(target).sum().item()
-            val_acc, val_loss= val(model, valloader)
+            val_acc, val_loss = self.val(model, self.valloader)
             print('Epoch: {epoch} | Train Loss: {loss} | Train Acc: {train_acc} | Val Loss: {val_loss} |Val Acc: {val_acc}'.format(
                 epoch = num, 
                 loss = lm.value()[0],
@@ -92,11 +115,16 @@ class training():
                 val_loss = val_loss, 
             ) )
             scheduler.step(val_acc)
-        final_acc, _= self.val(model, testloader)
-        print("Finished Training! The result is:{res}".format(
+            early(val_acc, model)
+        final_acc, _= self.val(model, self.testloader)
+        print("Finished Training! The result is: {res}".format(
             res = final_acc
         ))
-
+        model.savemodel(self.lr, 128, self.factor ,"Adam", "Plat", self.n_epoch)
+        w = t.randn(1, 3, 224, 224).cuda()
+        model.effnet.set_swish(memory_efficient=False)
+        t.onnx.export(model, w, "result.onnx", verbose= False)
+        self.onnxconversion()
 
 
     def val(self, model, dataloader):
@@ -109,7 +137,7 @@ class training():
             for n, (inputs, label) in enumerate(dataloader):
                 inputs, target = Variable(inputs).cuda(), Variable(label.long()).cuda()
                 score = model(inputs)
-                l = loss(score, target)
+                l = self.loss(score, target)
                 _, predicted = t.max(score.data, 1)
                 total += target.size(0)
                 correct += (predicted == target).sum().item()
@@ -119,3 +147,37 @@ class training():
         #     bacc = acc
         #     print("New Best Accurarcy:" + str(bacc))
         return (100 * correct/total), l
+    
+    def onnxconversion(self):
+        img = Image.open('airplane.jpg')
+        transform_train = transforms.Compose([
+            transforms.Resize((224)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])        
+        img = transform_train(img)
+        img = img.view(1, 3, 224, 224)
+        onnx_model =  onnx.load("result.onnx")
+        onnx.checker.check_model(onnx_model)
+        ort = onnxruntime.InferenceSession("result.onnx")
+        def to_numpy(tensor):
+            return tensor.detach().cpu().numpy()
+        # compute ONNX Runtime output prediction
+        ort_inputs = {ort.get_inputs()[0].name: to_numpy(img)}
+        t0 = time.clock()
+        ort_outs = ort.run(None, ort_inputs)
+        t1 = time.clock() - t0
+        result = list(self.classes)
+        print("Predicted result: {pres} | Actual Result: {ares} | Time Spent: {ts}".format(
+                ares=result[0], 
+                pres=result[ort_outs.index(max(ort_outs))], 
+                ts=t1
+        ))
+        
+
+        # compare ONNX Runtime and PyTorch results
+        # print("airplane", )
+
+if __name__ == "__main__":
+    tr = training(eff)
+    tr.train()    
